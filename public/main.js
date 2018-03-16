f7eb82158231cace59b007d96f4de814355e3995
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-03-16 08:54:26 -0400

    button change

--- public/main.js
+++ public/main.js
@@ -3,3 +3,21 @@ const main = () => {
 }
 
 document.addEventListener('DOMContentLoaded', main)
+
+
+//As a user I should be able to see three buttons for each player
+const gameStart = () => {
+  document.querySelector(".players .deal").addEventListener("click",()=>{
+    playerChoice(0,"rock")
+  })
+  document.querySelector(".players .hit").addEventListener("click",()=>{
+     playerChoice(0,"paper")
+   })
+   document.querySelector(".players .stay").addEventListener("click",()=>{
+     playerChoice(0,"scissors")
+   })
+  
+ 
+   document.querySelector(".go").addEventListener("click", choseWinner)
+ }
+ 
