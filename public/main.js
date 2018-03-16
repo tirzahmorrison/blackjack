93617fc0b290d42b00b581bc0984858c90618c67
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-03-16 09:30:10 -0400

    js deck

--- public/main.js
+++ public/main.js
@@ -19,6 +19,20 @@ const deck = new Array()
 const suits = ["spades", "diamonds", "clubs", "hearts"]
 const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
+//create deck
+const getDeck = () => {
+  const deck = new Array()
+for (const i = 0; i < suits.length; i++) {
+  for (const a = 0; a < values.length; x++) {
+    const card = () => {
+      Value: values[a], Suit; suits[i]
+    }
+    deck.push(card)
+  }
+}
+      return deck
+
+
 
 //As a user I should be able to see three buttons for each player
 const gameStart = () => {
