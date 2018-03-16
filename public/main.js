ad2fd0d5fb5bcb08d48751af08500310be202df3
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-03-16 14:58:54 -0400

    shuffle

--- public/main.js
+++ public/main.js
@@ -18,21 +18,36 @@ const blackjack = {
 const suits = ["spades", "clubs", "hearts", "diamonds"]
 const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
+
+
+
 //create deck
 const getDeck = () => {
-    const deck = []
-    for (let i = 0; i < suits.length; i++) {
-        for (let a = 0; a < values.length; a++) {
-            deck.push ({value: values[a], suit: suits[i]})
+  const deck = []
+  for (let i = 0; i < suits.length; i++) {
+    for (let a = 0; a < values.length; a++) {
+      deck.push({ value: values[a], suit: suits[i] })
+    }
+  }
+  return deck
 }
-}
-    return deck
-}
+
+
+
+
 
 //shuffle deck
+const shuffle = () => {
+  shufflefor (const i = 0; i < 1000; i++) {
+    const location1 = Math.floor((Math.random() * deck.length))
+    const location2 = Math.floor((Math.random() * deck.length))
+    const tmp = deck[location1]
 
+    deck[location1] = deck[location2]
+    deck[location2] = tmp
+  }
+}
 
-      
 
 
 
@@ -51,4 +66,61 @@ const gameStart = () => {
 
   document.querySelector("").addEventListener("click", choseWinner)
 }
+
+//build deck
+//interface Card { 
+//   suit: string
+// rank: string
+//limit: number 
+//} 
+
+//interface DeckOptions
+//{ 
+//   extend: Card[] 
+// suits: string[]
+//ranks: string[] 
+//multiply: number 
+//}
+
+//create deck
+//Class Deck
+//{ 
+//  private _opt: DeckOptions;
+
+//cards: Card[];
+//constructor(opt: Object = {})
+//{
+//this._opt = {
+//extend: opt['extend'] || [],
+//suits: opt['suits'] || ['spades', 'hearts', 'diamonds', 'clubs'],
+//ranks: opt['ranks'] || ['ace', 'king', 'queen', 'jack', '10', '9', '8', '7', '6', '5', '4', '3', '2'],
+//multiply: opt['multiply'] || 1
+//};
+
+//if (this._opt.multiply < 1)
+//this._opt.multiply = 1;
+//this.shuffle();
+//}
+
+//shuffle deck
+//shuffle()
+//{
+//this.cards = [];
+
+//for (let i = 0; i < this._opt.multiply; i++) {
+//for (let suit of this._opt.suits) {
+//for (let rank of this._opt.ranks) {
+//this.inlay({ suit: suit, rank: rank });
+//}
+//}
+//for (let card of this._opt.extend) {
+//if (!card.limit || i < card.limit)
+//this.inlay({ suit: card.suit, rank: card.rank });
+//}
+//}
+//}
+
+//}
+
+
 
