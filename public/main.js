c6537557404d31f4fab6551dc7409c663b0ab6af
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-03-18 22:42:32 -0400

    d

--- public/main.js
+++ public/main.js
@@ -244,7 +244,7 @@ class PlayerRenderer {
     this.watchedPlayer = watchedPlayer
     const playersArea = document.querySelector("#players-area")
     this.template = document.querySelector(".templates #player > section").cloneNode(true)
-    this.template.classList.add(this.watchedPlayer.name.replace(" ", ""))
+    this.template.classList.add(this.watchedPlayer.name.replace(/ /g, ""))
     playersArea.appendChild(this.template)
     this.template.querySelector(".playerBet").addEventListener("blur", () => { this.makeBet() })
     this.render()
